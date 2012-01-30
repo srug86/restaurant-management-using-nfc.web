91412047f67b1496a3b07bf2f6fb3d55f0e1e80f
srug86 <srug86@gmail.com>
2012-01-30 19:23:48 +0100

    - A?adidos los archivos de la aplicaci?n del servicio web.

---
+++ app/WebService/WebService/Service.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebService
+{
+    /// <summary>
+    /// Descripción breve de Service1
+    /// </summary>
+    [WebService(Namespace = "http://localhost/MobiCarta")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio Web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Service : System.Web.Services.WebService
+    {
+
+        [WebMethod(Description = "Convierte de Fahrenheit a Celsius, devuelve un valor Double")]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
