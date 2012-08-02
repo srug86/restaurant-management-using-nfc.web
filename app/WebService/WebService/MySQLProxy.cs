981430bdc3ec19ce0ca45baaca13a643a18eaa9a
srug86 <srug86@gmail.com>
2012-08-02 21:55:08 +0200

    - Cambiada la direcci?n de la base de datos a la de MAmI. - Cambiado el SGBD, ahora es MySQL.

---
+++ app/WebService/WebService/MySQLProxy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WebServices
+{
+    public sealed class MySQLProxy
+    {
+        //static readonly MySQLProxy instance = new MySQLProxy();
+
+        private MySqlCommand command;
+        private MySqlDataAdapter adapter;
+        private DataSet dataSet;
+
+        private string server, db, pwd;
+
+        static MySqlProxy() { }
+        MySqlProxy() { }
+
+        public static MySqlProxy Instance
+        {
+            get { return instance; }
+        }
+
+
+
+    }
+}
